a618f78ae7578efba26e492445c3293a9d9798a7
nico-eni <nicolas.martin2023@campus-eni.fr>
2023-12-11 10:26:50 +0100

    Initialisation du projet

---
+++ src/main/java/fr/eni/PizzaOnlinenico/PizzaOnlinenicoApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.PizzaOnlinenico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOnlinenicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOnlinenicoApplication.class, args);
+	}
+
+}
